a877e938ab646344030348cb60885ea63ed2bdc5
wice-armenia <info@wice.am>
2026-09-09 02:32:58 +0400

    Add previous awards links

--- content/config.ts
+++ content/config.ts
@@ -1,4 +1,4 @@
-export const WICE_HOME_URL = "https://wice.am/";
+export const WICE_HOME_URL = "https://www.wice.am/";
 export const WICE_EMAIL = "info@wice.am";
 export const WICE_FACEBOOK_URL = "https://web.facebook.com/wicearmenia/";
 
@@ -7,3 +7,9 @@ export const NOMINATION_FORM_URL =
 
 export const INFO_SESSION_ZOOM_URL =
   "https://us06web.zoom.us/j/86064448269?pwd=MKZQFXXaquomZmy9YWPong58cuBHkh.1";
+
+export const AWARDS_2022_URL =
+  "https://www.wice.am/Projects/%D5%B4%D6%80%D6%81%D5%A1%D5%B6%D5%A1%D5%AF%D5%A1%D5%A2%D5%A1%D5%B7%D5%AD%D5%B8%D6%82%D5%A9%D5%B5%D5%B8%D6%82%D5%B6/2022-Awards";
+
+export const AWARDS_2024_URL =
+  "https://www.wice.am/Projects/%D5%B4%D6%80%D6%81%D5%A1%D5%B6%D5%A1%D5%AF%D5%A1%D5%A2%D5%A1%D5%B7%D5%AD%D5%B8%D6%82%D5%A9%D5%B5%D5%B8%D6%82%D5%B6/2024-awards";
