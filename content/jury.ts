33b11566b1aa3de7acb962a8b2d0600c574b92f2
wice-armenia <info@wice.am>
2026-09-22 01:17:10 +0400

    Add jury member data

---
+++ content/jury.ts
@@ -0,0 +1,19 @@
+export type JuryMember = {
+  key: string;
+  name: string;
+  titleHy: string;
+  titleEn: string;
+  organization: string;
+  image: string;
+};
+
+export const juryMembers: JuryMember[] = [
+  {
+    key: "amelia-arreguin-prado",
+    name: "Amelia Arreguin Prado",
+    titleHy: "Համակարգող",
+    titleEn: "Coordinator",
+    organization: "CBD Women's Caucus",
+    image: "/images/jury/amelia-arreguin-prado.jpeg",
+  },
+];
