fa094f3f90acc6776bb4768a4615b00776aca56c
wice-armenia <info@wice.am>
2026-09-23 00:35:45 +0400

    Update jury members and details

--- content/jury.ts
+++ content/jury.ts
@@ -1,19 +1,33 @@
 export type JuryMember = {
   key: string;
-  name: string;
+  nameHy: string;
+  nameEn: string;
   titleHy: string;
   titleEn: string;
-  organization: string;
+  organizationHy: string;
+  organizationEn: string;
   image: string;
 };
 
 export const juryMembers: JuryMember[] = [
   {
-    key: "amelia-arreguin-prado",
-    name: "Amelia Arreguin Prado",
-    titleHy: "Համակարգող",
-    titleEn: "Coordinator",
-    organization: "CBD Women's Caucus",
-    image: "/images/jury/amelia-arreguin-prado.jpeg",
+  key: "amelia-arreguin-prado",
+  nameHy: "Amelia Arreguin Prado",
+  nameEn: "Amelia Arreguin Prado",
+  titleHy: "Համակարգող",
+  titleEn: "Coordinator",
+  organizationHy: "Կենսաբազմազանության մասին կոնվենցիայի կանանց խմբակցություն",
+  organizationEn: "CBD Women's Caucus",
+  image: "/images/jury/amelia-arreguin-prado.jpeg",
+},
+  {
+    key: "artem-kharazyan",
+    nameHy: "Արտեմ Խարազյան",
+    nameEn: "Artem Kharazyan",
+    titleHy: "Ազգային փորձագետ Հայաստանում",
+    titleEn: "Country Expert in Armenia",
+    organizationHy: "«Քաղաքապետերի դաշնագիր՝ Արևելք»",
+    organizationEn: "Covenant of Mayors East",
+    image: "/images/jury/artem-kharazyan.jpeg",
   },
 ];
